50ddc9d23fe61ff465e146dea3152e20540708e6
Ofelia Rahmanova <ofarx@yandex.ru>
2020-02-01 22:48:01 +0400

    homepage: list buttons js added, design fixes

--- assets/styles/js/restaurants-list.js
+++ assets/styles/js/restaurants-list.js
@@ -1,38 +1,69 @@
-var $grid = $('.grid').isotope({
-    itemSelector: '.place'
-  });
-  
-  // store filter for each group
-  var filters = {};
-  
-  $('.filters').on( 'click', '.btn', function( event ) {
-    var $button = $( event.currentTarget );
-    // get group key
-    var $buttonGroup = $button.parents('.button-group');
-    var filterGroup = $buttonGroup.attr('data-filter-group');
-    // set filter for group
-    filters[ filterGroup ] = $button.attr('data-filter');
-    // combine filters
-    var filterValue = concatValues( filters );
-    // set filter for Isotope
-    $grid.isotope({ filter: filterValue });
-  });
-  
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function( event ) {
-      $buttonGroup.find('.active').removeClass('active');
-      var $button = $( event.currentTarget );
-      $button.addClass('active');
-    });
-  });
-    
-  // flatten object by concatting values
-  function concatValues( obj ) {
-    var value = '';
-    for ( var prop in obj ) {
-      value += obj[ prop ];
-    }
-    return value;
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("place");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
+}
+
+filterSelection("all")
+function filterSelection(c) {
+  var y, j;
+  y = document.getElementsByClassName("place");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (j = 0; j < y.length; j++) {
+    RemoveClass(y[j], "show");
+    if (y[j].className.indexOf(c) > -1) AddClass(y[j], "show");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("btn-place");
+var btns = btnContainer.getElementsByClassName("button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+var btnContainer = document.getElementById("btn-food");
+var btns = btnContainer.getElementsByClassName("button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
