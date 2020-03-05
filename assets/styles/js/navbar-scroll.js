7f2e17f34cee047dd10101a12e902021d5b278e5
Ofelia Rahmanova <ofarx@yandex.ru>
2020-03-05 21:19:09 +0400

    data fill complete, bug fixes

--- assets/styles/js/navbar-scroll.js
+++ assets/styles/js/navbar-scroll.js
@@ -5,7 +5,7 @@ $(window).scroll(function () {
       $('#nav-bar').removeClass('nav-scrolled');
    }
 
-   if ($(this).scrollTop() > 325) {
+   if ($(this).scrollTop() > 280) {
       $('#nav-bar-auth').addClass('nav-scrolled');
    } else {
       $('#nav-bar-auth').removeClass('nav-scrolled');
